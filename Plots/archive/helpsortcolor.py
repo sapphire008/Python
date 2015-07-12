fb80407c05ebdb95f8647d269cd245534b4cdcac
Edward DongBo Cui <cui23327@gmail.com>
2015-07-12 17:12:09 -0400

    modified 071215

--- Plots/archive/helpsortcolor.py
+++ Plots/archive/helpsortcolor.py
@@ -8,7 +8,7 @@ DEBUG=True
 import re
 txtdir = 'C:/Users/Edward/Documents/Assignments/Scripts/Python/Plots/resource/colorbrewer2.txt'
 
-def readColorBrewers(txtdir, lineexp=r'(\w+)(?:,|$)\[([^\]]+)\](?:,|$)\[([^\]]+)\](?:,|$)',
+def readColorBrewers(txtdir, lineexp=r'(\w+)(?:,|$)\[([^\]]+)\](?:,|$)',
                      rgbexp = r'(\d+),(\d+),(\d+)'):
     """read color brewers text file"""
     #colors={}
@@ -16,16 +16,18 @@ def readColorBrewers(txtdir, lineexp=r'(\w+)(?:,|$)\[([^\]]+)\](?:,|$)\[([^\]]+)
         for line in fid:
             if not line.strip() or line[0] == "#":
                 continue  # skip comments or empty line
-            name, colorcomb, rgb  = re.findall(lineexp, line)[0]
+            if line.strip() == "@Stop":
+                break
+            name, rgb  = re.findall(lineexp, line)[0]
             # parse color list
             rgb_list = re.findall(rgbexp, rgb)
             # change into integer
             rgb_list = [tuple([int(r) for r in s]) for s in rgb_list]
-            
             # print
             rgbstr = ""
             for n, rgb in enumerate(rgb_list):
                 rgbstr += "(%d, (%d,%d,%d))," %(n, rgb[0],rgb[1],rgb[2])
-            # print("'%s' : OrderedDict([%s])" % (name, rgbstr[:-1]))
-            print("'%s',"% (name)),
+            print("'%s' : OrderedDict([%s])" % (name, rgbstr[:-1]))
+            # print("'%s',"% (name)),
     fid.close()
+    
