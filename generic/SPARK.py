2ecb285a09d88ee8363bfb10a7e76db58e5ae87d
Edward Cui <cui23327@gmail.com>
2020-12-29 10:36:43 -0500

    Convert pandas df to Spark df

--- generic/SPARK.py
+++ generic/SPARK.py
@@ -85,4 +85,35 @@ def countNullCols(df, cols=[]):
         return df.where(reduce(lambda x, y: x | y, \
             (F.col(x).isNull() for x in cols))).count()
     
+from pyspark.sql.types import *
 
+# Auxiliar functions
+# Pandas Types -> Sparks Types
+def equivalent_type(f):
+    if f == 'datetime64[ns]':
+        return DateType()
+    elif f == 'int64':
+        return LongType()
+    elif f == 'int32':
+        return IntegerType()
+    elif f == 'float64':
+        return FloatType()
+    else:
+        return StringType()
+
+def define_structure(string, format_type):
+    try: 
+        typo = equivalent_type(format_type)
+    except: 
+        typo = StringType()
+    return StructField(string, typo)
+
+#Given pandas dataframe, it will return a spark's dataframe
+def pandas_to_spark(df_pandas):
+    columns = list(df_pandas.columns)
+    types = list(df_pandas.dtypes)
+    struct_list = []
+    for column, typo in zip(columns, types): 
+        struct_list.append(define_structure(column, typo))
+    p_schema = StructType(struct_list)
+    return sqlContext.createDataFrame(df_pandas, p_schema)
