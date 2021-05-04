cbcd6f0bf6a3b2f45043dc5a55df8b4abc3539e7
Edward Cui <cui23327@gmail.com>
2021-05-04 13:32:56 -0400

    Create df_display.py

---
+++ Plots/df_display.py
@@ -0,0 +1,13 @@
+from IPython.display import display, HTML
+from IPython.display import display_html
+from itertools import chain,cycle
+def display_side_by_side(*args,titles=cycle([''])):
+    html_str=''
+    for df,title in zip(args, chain(titles,cycle(['</br>'])) ):
+        html_str+='<th style="text-align:center"><td style="vertical-align:top">'
+        html_str+=f'<h2>{title}</h2>'
+        html_str+=df.to_html().replace('table','table style="display:inline"')
+        html_str+='</td></th>'
+    display_html(html_str,raw=True)
+
+display_side_by_side(c[:25], c[25:50], c[50:75], c[75:100])
